588df1535b32c83c46240e8b62c559647d5d7ce7
BobDodPop <165052745+BobDodPop@users.noreply.github.com>
2026-09-25 11:10:58 -0500

    Added ritual pedestals

--- js/layers.js
+++ js/layers.js
@@ -279,13 +279,13 @@ addLayer("p", {
 })
 
 addLayer("b", {
-        name: "boosters", // This is optional, only used in a few places, If absent it just uses the layer id.
-        symbol: "B", // This appears on the layer's node. Default is the id with the first letter capitalized
+        name: "ritual pedestals", // This is optional, only used in a few places, If absent it just uses the layer id.
+        symbol: "RP", // This appears on the layer's node. Default is the id with the first letter capitalized
         position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-        color: "#6e64c4",
+        color: "#808080",
         requires() { return new Decimal(200).times((player.b.unlockOrder&&!player.b.unlocked)?5000:1) }, // Can be a function that takes requirement increases into account
-        resource: "boosters", // Name of prestige currency
-        baseResource: "points", // Name of resource prestige is based on
+        resource: "ritual pedestals", // Name of prestige currency
+        baseResource: "mana", // Name of resource prestige is based on
         baseAmount() {return player.points}, // Get the current amount of baseResource
         type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
 		branches: ["p"],
